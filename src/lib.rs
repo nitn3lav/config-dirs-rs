2bac088fad48be12a8036ad2db30b2011c9a8c54
Valentin Leistner <git@valentinleistner.com>
2024-06-16 21:38:41 +0200

    load from default config dirs (Linux, macOS)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,71 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    env, fs, io,
+    path::{Path, PathBuf},
+};
+
+use thiserror::Error;
+use tracing::{error, info};
+
+#[derive(Debug, Error)]
+pub enum Error<ParseError: std::error::Error> {
+    #[error("Failed to read config file: {0:#}")]
+    Io(#[from] io::Error),
+    #[error("Failed to parse config: {0:#}")]
+    Parse(ParseError),
+    #[error("Failed to load config from paths")]
+    NoPath,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn load<Config, E: std::error::Error>(
+    parse: impl Fn(&[u8]) -> Result<Config, E> + Copy,
+) -> Result<Config, Error<E>> {
+    if let Ok(path) = env::var("FFF_WUE_CMS_CONFIG") {
+        if let Some(path) = path_with_home_dir(&path) {
+            match load_from_path(path, parse) {
+                Err(Error::Io(e)) if matches!(e.kind(), io::ErrorKind::NotFound) => {}
+                v => return v,
+            }
+        }
+    }
+    let paths = [
+        concat!("~/.config/", env!("CARGO_CRATE_NAME"), "/config.toml"),
+        concat!("/etc/", env!("CARGO_CRATE_NAME"), "/config.toml"),
+        concat!("/usr/local/etc/", env!("CARGO_CRATE_NAME"), "/config.toml"),
+        concat!(
+            "~/Library/Preferences/",
+            env!("CARGO_CRATE_NAME"),
+            "/config.toml"
+        ),
+        concat!(
+            "/Library/Preferences/",
+            env!("CARGO_CRATE_NAME"),
+            "/config.toml"
+        ),
+    ];
+    for path in paths {
+        if let Some(path) = path_with_home_dir(path) {
+            match load_from_path(path, parse) {
+                Err(Error::Io(e)) if matches!(e.kind(), io::ErrorKind::NotFound) => {}
+                v => return v,
+            }
+        }
+    }
+    error!("Failed to load config");
+    Err(Error::NoPath)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn load_from_path<Config, E: std::error::Error>(
+    path: impl AsRef<Path>,
+    parse: impl Fn(&[u8]) -> Result<Config, E>,
+) -> Result<Config, Error<E>> {
+    info!("Loading config from {}", path.as_ref().to_string_lossy());
+    parse(&fs::read(path)?).map_err(Error::Parse)
+}
+
+fn path_with_home_dir(path: &str) -> Option<PathBuf> {
+    if path.starts_with("~/") {
+        dirs::home_dir().map(|v| v.join(&path[2..]))
+    } else {
+        Some(PathBuf::from(path))
     }
 }
